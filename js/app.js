5d2d6849400d5cfb4e665b3b29e15327ecf3ba19
Sarfaraz <khansarfaraz2141@gmail.com>
2021-09-01 13:58:31 +0530

    Format code using Prettier

--- js/app.js
+++ js/app.js
@@ -1,104 +1,104 @@
 //allows line breaks with return button
 marked.setOptions({
-    breaks: true
+  breaks: true,
 });
 
 //inserts target="_blank" into href tags
 const renderer = new marked.Renderer();
 renderer.link = function (href, title, text) {
-    return `<a target="_blank" href="${href}">${text}` + "</a>";
+  return `<a target="_blank" href="${href}">${text}` + "</a>";
 };
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            markdown: placeholder,
-            editorMaximized: false,
-            previewMaximized: false
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleEditorMaximize = this.handleEditorMaximize.bind(this);
-        this.handlePreviewMaximize = this.handlePreviewMaximize.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      markdown: placeholder,
+      editorMaximized: false,
+      previewMaximized: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleEditorMaximize = this.handleEditorMaximize.bind(this);
+    this.handlePreviewMaximize = this.handlePreviewMaximize.bind(this);
+  }
 
-    handleChange(e) {
-        this.setState({
-            markdown: e.target.value
-        });
-    }
-    handleEditorMaximize() {
-        this.setState({
-            editorMaximized: !this.state.editorMaximized
-        });
-    }
-    handlePreviewMaximize() {
-        this.setState({
-            previewMaximized: !this.state.previewMaximized
-        });
-    }
+  handleChange(e) {
+    this.setState({
+      markdown: e.target.value,
+    });
+  }
+  handleEditorMaximize() {
+    this.setState({
+      editorMaximized: !this.state.editorMaximized,
+    });
+  }
+  handlePreviewMaximize() {
+    this.setState({
+      previewMaximized: !this.state.previewMaximized,
+    });
+  }
 
-    render() {
-        const classes = this.state.editorMaximized
-            ? ["editorWrap maximized", "previewWrap hide", "fa fa-compress"]
-            : this.state.previewMaximized
-                ? ["editorWrap hide", "previewWrap maximized", "fa fa-compress"]
-                : ["editorWrap", "previewWrap", "fa fa-arrows-alt"];
+  render() {
+    const classes = this.state.editorMaximized
+      ? ["editorWrap maximized", "previewWrap hide", "fa fa-compress"]
+      : this.state.previewMaximized
+      ? ["editorWrap hide", "previewWrap maximized", "fa fa-compress"]
+      : ["editorWrap", "previewWrap", "fa fa-arrows-alt"];
 
-        return (
-            <div>
-                <div className={classes[0]}>
-                    <Toolbar
-                        icon={classes[2]}
-                        onClick={this.handleEditorMaximize}
-                        text="Enter Markdown"
-                    />
-                    <Editor markdown={this.state.markdown} onChange={this.handleChange} />
-                </div>
-                <div className="converter" />
-                <div className={classes[1]}>
-                    <Toolbar
-                        icon={classes[2]}
-                        onClick={this.handlePreviewMaximize}
-                        text="Result"
-                    />
-                    <Preview markdown={this.state.markdown} />
-                </div>
-            </div>
-        );
-    }
+    return (
+      <div>
+        <div className={classes[0]}>
+          <Toolbar
+            icon={classes[2]}
+            onClick={this.handleEditorMaximize}
+            text="Enter Markdown"
+          />
+          <Editor markdown={this.state.markdown} onChange={this.handleChange} />
+        </div>
+        <div className="converter" />
+        <div className={classes[1]}>
+          <Toolbar
+            icon={classes[2]}
+            onClick={this.handlePreviewMaximize}
+            text="Result"
+          />
+          <Preview markdown={this.state.markdown} />
+        </div>
+      </div>
+    );
+  }
 }
 
 const Toolbar = (props) => {
-    return (
-        <div className="toolbar">
-            <i className="fa fa-github" title="GoogolDKhan" />
-            {props.text}
-            <i className={props.icon} onClick={props.onClick} />
-        </div>
-    );
+  return (
+    <div className="toolbar">
+      <i className="fa fa-github" title="GoogolDKhan" />
+      {props.text}
+      <i className={props.icon} onClick={props.onClick} />
+    </div>
+  );
 };
 
 const Editor = (props) => {
-    return (
-        <textarea
-            id="editor"
-            onChange={props.onChange}
-            type="text"
-            value={props.markdown}
-        />
-    );
+  return (
+    <textarea
+      id="editor"
+      onChange={props.onChange}
+      type="text"
+      value={props.markdown}
+    />
+  );
 };
 
 const Preview = (props) => {
-    return (
-        <div
-            dangerouslySetInnerHTML={{
-                __html: marked(props.markdown, { renderer: renderer })
-            }}
-            id="preview"
-        />
-    );
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: marked(props.markdown, { renderer: renderer }),
+      }}
+      id="preview"
+    />
+  );
 };
 
 const placeholder = `
